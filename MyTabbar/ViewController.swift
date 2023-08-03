e7d27bef24570a861d29fa2e85c440d3a227c2c8
Anton Akhmedzyanov <anton.akhmedzyanov@gmail.com>
2023-08-03 22:09:31 +0300

    Initial Commit

---
+++ MyTabbar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTabbar
+//
+//  Created by Anton Akhmedzyanov on 03.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
